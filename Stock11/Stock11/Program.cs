7ee66e89875796eda04e49b94872ae0f12bc9910
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-03-28 19:22:45 -0400

    [Ruchita] Add:  stock report

---
+++ Stock11/Stock11/Program.cs
@@ -0,0 +1,37 @@
+﻿using static Stock11.Stock;
+
+namespace Stock11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to stock management system");
+            
+
+            StockPortfolio portfolio = new StockPortfolio();
+            //To print for 5 stocks
+            for (int i = 1; i <=5; i++)
+            {
+              
+                Console.WriteLine("Enter the StockName:");
+                string stockname = Console.ReadLine();
+
+                Console.WriteLine("Enter the number of shares");
+                int numOfShares = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the shares price ");
+                decimal sharePrice = Convert.ToDecimal(Console.ReadLine());
+                Stock stock = new Stock();
+                stock.StockName = stockname;
+                stock.NumOfShares = numOfShares;
+                stock.SharePrice = (float)sharePrice;
+
+               portfolio.AddStock(stock);
+            }
+
+           portfolio.PrintStockReport();
+
+        }
+    }
+}
---
+++ Stock11/Stock11/Stock.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock11
+{
+    internal class Stock
+    {
+
+        public string StockName { get; set; }
+        public int NumOfShares { get; set; }
+        public float SharePrice { get; set; }
+
+        public float GetStockValue()
+        {
+            //It will calculate the stock value 
+            return NumOfShares * SharePrice;
+        }
+
+        public class StockPortfolio
+        {
+            //create list of stocks 
+            List<Stock> stocks = new List<Stock>();
+
+            //add values to stock 
+
+            public void AddStock(Stock stock)
+            {
+                stocks.Add(stock);
+            }
+            public float GetTotalStockValue()
+            {
+                float totalStockValue = 0;
+
+                //Total stock value= total stock +stockvalue 
+                foreach (Stock stock in stocks)
+                {
+                    totalStockValue = totalStockValue + stock.GetStockValue();
+                }
+
+                return totalStockValue;
+            }
+            //print stock report 
+            public void PrintStockReport()
+            {
+                Console.WriteLine("*************Welcome To Stock Management***************");
+                Console.WriteLine("**********Stock Report:****************");
+
+
+                foreach (Stock stock in stocks)
+                {
+                    float value = stock.GetStockValue();
+                    Console.WriteLine(" StockName:"+stock.StockName);
+                    Console.WriteLine(" NumberofShares: "+stock.NumOfShares);
+                    Console.WriteLine(" PriceOfShare: "+stock.SharePrice);
+                    Console.WriteLine("--------");
+                }
+
+                //Calculate total value of stocks 
+                float totalStocks = GetTotalStockValue();
+
+                Console.WriteLine("Total number of stocks:" + totalStocks);
+                Console.WriteLine();
+                
+
+
+        }
+        }
+    }
+
+}
